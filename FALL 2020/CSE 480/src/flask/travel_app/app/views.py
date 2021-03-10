9a2d3bcc05affcdf0bfccb6a9e8ba544e4d91d11
Satyaki Das <satyakidaslesnar@gmail.com>
2021-03-11 01:47:30 +0600

    New Course Outline is being prepared

--- FALL 2020/CSE 480/src/flask/travel_app/app/views.py
+++ FALL 2020/CSE 480/src/flask/travel_app/app/views.py
@@ -1,7 +1,18 @@
-from flask import render_template
+from flask import render_template, request
 from app import t_app
 
 @t_app.route("/")
 @t_app.route("/index")
 def index():
     return render_template("index.html")
+
+@t_app.route("/show_form")
+def show_form():
+    return render_template("form_demo.html")
+
+@t_app.route("/form_demo", methods=["GET", "POST"])
+def form_demo():
+    studentID = request.args.get("student-id", "181011001", type=str)
+    print(studentID)
+
+    return "Thank You"
---
+++ SPRING 2021/CSE 304/src/db_connect.cpp
@@ -0,0 +1,14 @@
+
+
+
+#include <iostream>
+
+#include "mysql_connection.h"
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
